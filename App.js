0a5beb5a4aea87d41c10666ce2276dfe4e10b37c
Beren Yagmur Donmez <bydonmez@bu.edu>
2023-06-22 10:46:24 +0300

    Update App.js

--- App.js
+++ App.js
@@ -112,18 +112,19 @@ const styles = StyleSheet.create({
  
  userImage: {
   position: 'absolute',
-  top: -10, // Adjust the value to position the user image vertically
-  left: -10, // Adjust the value to position the user image horizontally
+  top: 1, // Adjust the value to position the user image vertically
+  left: 1, // Adjust the value to position the user image horizontally
   width: 50,
   height: 50,
   resizeMode: 'cover',
 },
 profileInfo: {
   position: 'absolute',
+  top: 10, // Adjust the value to position the profile info vertically
+  left: 10, // Adjust the value to position the profile info horizontally
   flexDirection: 'row',
-  marginTop: 20,
-  marginLeft: 20,
 },
+
 
 
 });
